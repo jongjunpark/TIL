e78e243067cb29f6198e7dc52c19720b6e569a10
parkjongjun <poiufgin7373@naver.com>
2020-07-07 13:39:00 +0900

    add test problem

--- Public/problem/test2.py
+++ Public/problem/test2.py
@@ -1,31 +1,30 @@
-import sys,heapq
+def solution(board, moves):
+    answer = 0
+    basket = []
+    doll_catch(board, moves, 0, basket, len(moves))
+    answer = len(basket)
+    print(answer)
+    return answer
 
-sys.stdin = open('input.txt', 'r')
-
-def road_sum(i,j,s):
-    global min_sum
-    if s > min_sum:
+def doll_catch(arr, moves, t, basket, f):
+    if t >= f:
         return
-    if i == N-1 and j == N-1:
-        min_sum = s
-        return
-    if i+1 < N:
-        road_sum(i+1,j,s+road[i+1][j])
-    if j+1 < N:
-        road_sum(i,j+1,s+road[i][j+1])
-    if i-1 >= 0:
-        road_sum(i-1,j,s+road[i-1][j])
-    if j-1 >= 0:
-        road_sum(i,j-1,s+road[i][j-1])
+    for i in range(len(arr)):
+        if arr[i][moves[t]-1]:
+            if basket and basket[-1] == arr[i][moves[t]-1]:
+                basket.pop()
+                arr[i][moves[t]-1] = 0
+                while len(basket)>=2 and basket[-1] != basket[-2]:
+                    basket.pop()
+                    basket.pop()
+            else:
+                basket.append(arr[i][moves[t]-1])
+                arr[i][moves[t]-1] = 0
+            break
+    doll_catch(arr, moves, t+1, basket, f)
 
-
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input())
-    road = [list(map(int, input())) for _ in range(N)]
-    min_sum = 0
-    for i in range(N):
-        min_sum += road[0][i]
-        min_sum += road[i][-1]
-    road_sum(0,0,0)
-    print(min_sum)
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+solution(board, moves)
+            
+            
