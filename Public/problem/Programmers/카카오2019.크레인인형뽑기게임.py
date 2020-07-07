d1f5b71915f1961dc783868b8290c84a89666c6c
parkjongjun <poiufgin7373@naver.com>
2020-07-07 14:02:27 +0900

    kakao2020 doll problem

--- Public/problem/Programmers/카카오2019.크레인인형뽑기게임.py
+++ Public/problem/Programmers/카카오2019.크레인인형뽑기게임.py
@@ -26,9 +26,3 @@ def doll_catch(arr, moves, t, basket, f, s):
                 arr[i][moves[t]-1] = 0
             break
     doll_catch(arr, moves, t+1, basket, f, cnt)
-
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-moves = [1,5,3,5,1,2,1,4]
-solution(board, moves)
-            
-            
